e6b5dbaebab1e9eb898331d72cebd23fffcab21c
Ranjeet Rathour <ranjeet.rathour@xcelore.com>
2025-11-29 12:47:39 +0530

    patient service api working

--- common-service/src/main/java/com/health/CommanServiceApplication.java
+++ common-service/src/main/java/com/health/CommanServiceApplication.java
@@ -1,6 +1,6 @@
 package com.health;
 
-public class Main {
+public class CommanServiceApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
